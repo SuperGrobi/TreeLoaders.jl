50776f6ece68627bf9b6d69a1d4f9ae0b01119b8
TreeLoaders.jl <henrik-wolf@freenet.de>
2023-08-06 20:04:33 +0200

    stuff

--- test/current.jl
+++ test/current.jl
@@ -1,14 +1,31 @@
 using TreeLoaders
 using DataFrames
+using CSV
 using Plots
 using ArchGDAL
 using SpatialIndexing
 using Folium
+using Extents
 using CoolWalksUtils
 
-path = "/Users/henrikwolf/Desktop/Masterarbeit/CoolWalksAnalysis/data/exp_raw/manhattan/"
+path = "/Users/henrikwolf/Desktop/Masterarbeit/CoolWalksAnalysis/data/exp_raw/spain/barcelona/trees/2023_1T_OD_Arbrat_Parcs_BCN.csv"
+path = "/Users/henrikwolf/Desktop/Masterarbeit/CoolWalksAnalysis/data/exp_raw/spain/valencia/arbratge-arbolado.geojson"
+
+load_valencia_trees(path)
+
 
 tr = CSV.read(joinpath(path, "2015_Street_Tree_Census_-_Tree_Data.csv"), DataFrame)
+
+TreeLoaders.apply_extent!(tr, Extent(X=(-73.99339543820753, -73.95967802174474), Y=(40.69935632811247, 40.727061432104165)))
+
+CSV.write(joinpath(homedir(), "Desktop/Masterarbeit/packages/TreeLoaders.jl/test/data/new_york_treesample.csv"), tr)
+
+tr.borough |> Set
+
+
+tr.borough |> Set .|> String
+
+metadata(tr)
 
 names(tr)
 
@@ -16,7 +33,11 @@ filter(:borough => ==("Manhattan"), tr)
 
 tr
 
-trees = load_new_york_trees(joinpath(path, "2015_Street_Tree_Census_-_Tree_Data.csv"), borough="Manhattan")
+trees = load_new_york_trees(joinpath(homedir(), "Desktop/Masterarbeit/packages/TreeLoaders.jl/test/data/new_york_treesample.csv"), borough="Brooklyn")
+trees = load_new_york_trees(joinpath(homedir(), "Desktop/Masterarbeit/packages/TreeLoaders.jl/test/data/new_york_treesample.csv"))
+
+
+metadata(trees)
 
 names(trees)
 vcat(trees, trees)
@@ -42,45 +63,8 @@ begin
 end
 
 
+trees = load_nottingham_trees(joinpath(homedir(), "Desktop/Masterarbeit/packages/TreeLoaders.jl/test/data/nottingham_treesample.csv"); extent=Extent(X=(-1.2, -1.165), Y=(52.89, 52.92)))
 
+names(trees)
 
-trees = load_nottingham_trees(joinpath(homedir(), path); bbox=(minlon=-1.2, minlat=52.89, maxlon=-1.165, maxlat=52.92));
-
-shadows = cast_shadow(trees, tree_param_getter_nottingham, [1, 0.4, 0.5])
-
-TreeLoaders.project_local!(trees.pointgeom, metadata(trees, "center_lon"), metadata(trees, "center_lat"))
-
-trees
-fr = trees_full[1, :]
-fr.id = 0
-fr
-trees_full
-trees_reduced
-scatter(trees.pointgeom, ms=5rand(length(trees.pointgeom))', ratio=1)
-
-
-rt = RTree{Float64,2}(Int, String)
-for row in eachrow(trees)
-    x = ArchGDAL.getx(row.pointgeom, 0)
-    y = ArchGDAL.gety(row.pointgeom, 0)
-    insert!(rt, SpatialIndexing.Rect((x, y), (x, y)), row.id, string(row.id))
-end
-
-n = 1
-buffer = 1e-5
-x = ArchGDAL.getx(trees.pointgeom[n], 0)
-y = ArchGDAL.gety(trees.pointgeom[n], 0)
-
-intersection = intersects_with(rt, SpatialIndexing.Rect((x - buffer, y - buffer), (x + buffer, y + buffer)));
-ids = [i.id for i in intersection]
-
-
-rt1 = RTree{Float64,2}(Int, Int)
-for i in 1:40
-    x = rand()
-    y = rand()
-    insert!(rt1, SpatialIndexing.Rect((x, y), (x, y)), i, i)
-end
-
-
-nrow(trees)
+TreeLoaders.check_tree_dataframe_integrity(trees)
