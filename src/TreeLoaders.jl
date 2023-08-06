315edb535720c897bf603da7776feb920062850e
TreeLoaders.jl <henrik-wolf@freenet.de>
2023-08-06 20:04:22 +0200

    added quotes around printing

--- src/TreeLoaders.jl
+++ src/TreeLoaders.jl
@@ -46,13 +46,13 @@ function check_tree_dataframe_integrity(df)
     colnames = Symbol.(names(df))
     needed_names = [:id, :lon, :lat, :height, :radius, :geometry]
     for i in needed_names
-        @assert i in colnames "no column of df is named $i."
+        @assert i in colnames "no column of df is named \"$i\"."
     end
     for g in df.geometry
         @assert g isa ArchGDAL.IGeometry{ArchGDAL.wkbPoint} "not all entries in the :geometry column are ArchGDAL points."
     end
 
-    @assert "observatory" in keys(metadata(df)) "the dataframe has no observatory metadata."
+    @assert "observatory" in keys(metadata(df)) "the dataframe has no \"observatory\" metadata."
     @assert metadata(df, "observatory") isa ShadowObservatory "the provided obervatory is not of type ShadowObservatory."
 end
 
